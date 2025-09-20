4d7ee52fb7e3038b73ca7156a5ef7167c3e14f28
Michael Myeko <myekodeveloper.rammy@gmail.com>
2025-09-20 15:38:35 +0300

    Michael Myeko
    All right reserved by Michael Myeko

---
+++ Calculator App/script.js
@@ -0,0 +1,38 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (item.id == "equal") {
+      if (display.innerText !== "") {
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerText = "Error!";
+          setTimeout(() => (display.innerText = ""), 2000);
+        }
+      } else {
+        display.innerText = "Empty!";
+        setTimeout(() => (display.innerText = ""), 2000);
+      }
+    } else {
+      display.innerText += item.id;
+    }
+  });
+});
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggle-icon");
+let isDark = true;
+
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
